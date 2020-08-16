980d38c9df966f2d4e06945d3357ef74da77137d
Jigish Gohil <cyberorg@cyberorg.info>
2020-08-16 13:09:46 +0530

    mss customizations

--- src/android_service.py
+++ src/android_service.py
@@ -6,6 +6,7 @@ import os
 import pew.ui
 import shutil
 import time
+import threading
 
 from config import FLASK_PORT
 
@@ -36,10 +37,29 @@ if not os.path.exists(HOME_PATH) and os.path.exists(HOME_TEMPLATE_PATH):
 # ensure the service stays running by "foregrounding" it with a persistent notification
 make_service_foreground("Kolibri is running...", "Click here to resume.")
 
+#MSS sync
+def run_sync():
+    threading.Timer(60.0, run_sync).start()
+    from kolibri.utils.cli import main
+    from configparser import ConfigParser
+    KOLIBRI_HOME=os.environ.get("KOLIBRI_HOME")
+    syncini_path = os.path.join(KOLIBRI_HOME, "syncoptions.ini")
+    try:
+        file = open(syncini_path, 'r')
+    except IOError:
+        return
+    configur = ConfigParser()
+    configur.read(syncini_path)
+    syncuser=configur.get('MSS','SYNC_USER')
+    syncpass=configur.get('MSS','SYNC_PASS')
+    syncfacility=configur.get('MSS','SYNC_FACILITY')
+    main(["manage", "sync", "--baseurl", "http://content.myscoolserver.in/", "--username", syncuser, "--password", syncpass, "--facility", syncfacility, "--verbosity", "3"])
+
 # start the kolibri server as a thread
 thread = pew.ui.PEWThread(target=start_kolibri_server)
 thread.daemon = True
 thread.start()
+run_sync()
 
 # start a parallel Flask server as a backchannel for triggering events
 flaskapp = flask.Flask(__name__)
