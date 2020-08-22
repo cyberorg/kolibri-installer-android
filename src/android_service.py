d9e465945e55c28aa9772d3884ae822b4c0f258e
Jigish Gohil <cyberorg@cyberorg.info>
2020-08-22 19:23:28 +0530

    rearrange

--- src/android_service.py
+++ src/android_service.py
@@ -38,66 +38,10 @@ if not os.path.exists(HOME_PATH) and os.path.exists(HOME_TEMPLATE_PATH):
 # ensure the service stays running by "foregrounding" it with a persistent notification
 make_service_foreground("Kolibri is running...", "Click here to resume.")
 
-def facility_sync(syncdelay, syncserver, syncuser, syncpass, syncfacilityid):
-    pid = os.fork()
-    if pid == 0:
-        main(["manage", "sync", "--baseurl", syncserver, "--username", syncuser, "--password", syncpass, "--facility", syncfacilityid, "--verbosity", "3"])
-    else:
-        os.waitpid(pid, 0)
-
-def run_sync():
-    from kolibri.utils.cli import main
-    f = open(os.devnull, 'w')
-    sys.stdout = f
-#    logging.basicConfig(level=logging.INFO)
-#    logging.disable(logging.INFO)
-#    logging.disable(logging.WARNING)
-    from configparser import ConfigParser
-    KOLIBRI_HOME = os.environ.get("KOLIBRI_HOME")
-    syncini_file = os.path.join(KOLIBRI_HOME, "syncoptions.ini")
-    configur = ConfigParser()
-    try:
-        file = open(syncini_file, 'r')
-    except IOError:
-        configur['DEFAULT'] = { 'SYNC_ON': 'True',
-                                'SYNC_SERVER': 'content.myscoolserver.in',
-                                'SYNC_USER': 'syncuser',
-                                'SYNC_DELAY': '900.0'
-                                }
-        with open(syncini_file, 'w') as configfile:
-            configur.write(configfile)
-        return
-    configur.read(syncini_file)
-    syncon = configur.getboolean('DEFAULT', 'SYNC_ON')
-    if (syncon):
-        from django.core.management import execute_from_command_line
-        execute_from_command_line(sys.argv)
-        from kolibri.core.auth.models import Facility
-        syncfacilities = Facility.objects.filter()
-        syncuser = configur.get('DEFAULT', 'SYNC_USER')
-        syncdelay = configur.getfloat('DEFAULT', 'SYNC_DELAY')
-        syncserver = configur.get('DEFAULT', 'SYNC_SERVER')
-        if syncfacilities:
-            for syncfacility in syncfacilities:
-                syncfacilityid = syncfacility.id
-                syncpass = "sync" + syncfacilityid
-                if syncfacilityid in configur:
-                    syncuser = configur.get(syncfacilityid, 'SYNC_USER')
-                    syncpass = configur.get(syncfacilityid, 'SYNC_PASS')
-                    syncdelay = configur.getfloat(syncfacilityid, 'SYNC_DELAY')
-                    syncserver = configur.get(syncfacilityid, 'SYNC_SERVER')
-                pid = os.fork()
-                if pid == 0:
-                    main(["manage", "sync", "--baseurl", syncserver, "--username", syncuser, "--password", syncpass, "--facility", syncfacilityid, "--verbosity", "3"])
-                else:
-                    os.waitpid(pid, 0)
-        threading.Timer(syncdelay, run_sync).start()
-
 # start the kolibri server as a thread
 thread = pew.ui.PEWThread(target=start_kolibri_server)
 thread.daemon = True
 thread.start()
-run_sync()
 
 # start a parallel Flask server as a backchannel for triggering events
 flaskapp = flask.Flask(__name__)
---
+++ src/msssync.py
@@ -0,0 +1,66 @@
+import initialization
+
+import time
+import logging
+import os
+import sys
+import threading
+
+from kolibri.utils.cli import main
+
+def facility_sync(syncserver, syncuser, syncpass, syncfacilityid):
+    pid = os.fork()
+    if pid == 0:
+        main(["manage", "sync", "--baseurl", syncserver, "--username", syncuser, "--password", syncpass, "--facility", syncfacilityid, "--verbosity", "3"])
+    else:
+        os.waitpid(pid, 0)
+
+# MSS Cloud sync for multifacilities on user device
+def run_sync():
+#    logging.basicConfig(level=logging.INFO)
+#    logging.disable(logging.INFO)
+#    logging.disable(logging.WARNING)
+    from configparser import ConfigParser
+    KOLIBRI_HOME = os.environ.get("KOLIBRI_HOME")
+    syncini_file = os.path.join(KOLIBRI_HOME, "syncoptions.ini")
+    configur = ConfigParser()
+    try:
+        file = open(syncini_file, 'r')
+    except IOError:
+        configur['DEFAULT'] = { 'SYNC_ON': 'True',
+                                'SYNC_SERVER': 'content.myscoolserver.in',
+                                'SYNC_USER': 'syncuser',
+                                'SYNC_DELAY': '900.0'
+                                }
+        with open(syncini_file, 'w') as configfile:
+            configur.write(configfile)
+
+    configur.read(syncini_file)
+    syncon = configur.getboolean('DEFAULT', 'SYNC_ON')
+    syncdelay = configur.getfloat('DEFAULT', 'SYNC_DELAY')
+    if (syncon):
+        threading.Timer(syncdelay, run_sync).start()
+        from django.core.management import execute_from_command_line
+        sys.__stdout__ = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+        execute_from_command_line(sys.argv)
+        sys.stdout = sys.__stdout__
+        from kolibri.core.auth.models import Facility
+        syncfacilities = Facility.objects.filter()
+        syncuser = configur.get('DEFAULT', 'SYNC_USER')
+        syncserver = configur.get('DEFAULT', 'SYNC_SERVER')
+        if syncfacilities:
+            logging.info(syncfacilities)
+            for syncfacility in syncfacilities:
+                syncfacilityid = syncfacility.id
+                syncpass = "sync" + syncfacilityid
+                if syncfacilityid in configur:
+                    syncuser = configur.get(syncfacilityid, 'SYNC_USER')
+                    try:
+                        syncpass = configur.get(syncfacilityid, 'SYNC_PASS')
+                    except:
+                        syncpass = "sync" + syncfacilityid
+                    syncdelay = configur.getfloat(syncfacilityid, 'SYNC_DELAY')
+                    syncserver = configur.get(syncfacilityid, 'SYNC_SERVER')
+                facility_sync(syncserver, syncuser, syncpass, syncfacilityid)
+
