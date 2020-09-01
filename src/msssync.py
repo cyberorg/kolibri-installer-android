10966876853a572c3183268159fee2aadeb9cfaa
Jigish Gohil <cyberorg@cyberorg.info>
2020-09-01 09:44:28 +0530

    no user credentials needed for sync

--- src/msssync.py
+++ src/msssync.py
@@ -8,10 +8,10 @@ import threading
 
 from kolibri.utils.cli import main
 
-def facility_sync(syncserver, syncuser, syncpass, syncfacilityid):
+def facility_sync(syncserver, syncfacilityid):
     pid = os.fork()
     if pid == 0:
-        main(["manage", "sync", "--baseurl", syncserver, "--username", syncuser, "--password", syncpass, "--facility", syncfacilityid, "--verbosity", "3"])
+        main(["manage", "sync", "--baseurl", syncserver, "--facility", syncfacilityid, "--verbosity", "3"])
     else:
         os.waitpid(pid, 0)
 
@@ -29,7 +29,6 @@ def run_sync():
     except IOError:
         configur['DEFAULT'] = { 'SYNC_ON': 'True',
                                 'SYNC_SERVER': 'content.myscoolserver.in',
-                                'SYNC_USER': 'syncuser',
                                 'SYNC_DELAY': '900.0'
                                 }
         with open(syncini_file, 'w') as configfile:
@@ -47,20 +46,12 @@ def run_sync():
         sys.stdout = sys.__stdout__
         from kolibri.core.auth.models import Facility
         syncfacilities = Facility.objects.filter()
-        syncuser = configur.get('DEFAULT', 'SYNC_USER')
         syncserver = configur.get('DEFAULT', 'SYNC_SERVER')
         if syncfacilities:
             logging.info(syncfacilities)
             for syncfacility in syncfacilities:
                 syncfacilityid = syncfacility.id
-                syncpass = "sync" + syncfacilityid
                 if syncfacilityid in configur:
-                    syncuser = configur.get(syncfacilityid, 'SYNC_USER')
-                    try:
-                        syncpass = configur.get(syncfacilityid, 'SYNC_PASS')
-                    except:
-                        syncpass = "sync" + syncfacilityid
-                    syncdelay = configur.getfloat(syncfacilityid, 'SYNC_DELAY')
                     syncserver = configur.get(syncfacilityid, 'SYNC_SERVER')
-                facility_sync(syncserver, syncuser, syncpass, syncfacilityid)
+                facility_sync(syncserver, syncfacilityid)
 
